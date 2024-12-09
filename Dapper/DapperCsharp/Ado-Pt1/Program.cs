10a93e2aee55a6c41062709cb9cd9ddb9c90ed91
Samuel Ribeiro <samuel.ribeiro77f@gmail.com>
2024-12-08 20:34:19 -0400

    Aprendendo o básico sobre o ADO .NET

---
+++ Dapper/DapperCsharp/Ado-Pt1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+namespace Ado;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost,9090;Database=balta;User ID=sa;Password=;1q2w3e4r@#$";
+        //Microsoft.Data.SqlClient pacote para acessar o banco de dados baixado via (NuGet)
+        
+        //Em vez de usar dessa formar, podemos usar o using para garantir que a conexão será encerrada
+        /*
+         * var connection = new SqlConnection(connectionString);
+         * connection.Open(); Abre a conexão com o banco de dados
+         * connection.Close(); Fecha a conexão com o banco de dados
+         * connection.Dispose(); Irá destruir o objeto e fechar a conexão
+        */
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            Console.WriteLine("Conectando ao banco de dados...");
+        }
+        
+        /* -----------------------------------------------------------
+         * Ao abrir uma conexão é sempre bom você fechar após feito
+         * o que deveria, se você deixar as conexões abertas
+         * o SQL Server irá dar gargalos. 
+         * ----------------------------------------------------------- */
+        
+        /* -----------------------------------------------------------
+         * Ao abrir uma conexão faça tudo que precisa ser feito de uma
+         * vez, pelo fato de ter um tempo minimo para a conexão abrir,
+         * ao fazer isso você economizará tempo
+         * ----------------------------------------------------------- */
+    }
+}
+
---
+++ Dapper/DapperCsharp/Ado_Pt2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+namespace Ado_Pt2;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost,9090;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+        
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open(); // Abre explicitamente a conexão antes de usá-la
+
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection; // Associa o comando à conexão
+                command.CommandType = CommandType.Text; // Define o tipo de comando SQL
+                command.CommandText = "SELECT [Id], [Title] FROM [Category]"; // Define o comando SQL
+
+                using var reader = command.ExecuteReader(); // Executa o comando e obtém um leitor
+            
+                while (reader.Read()) // Enquanto houver resultados, o 'while' será executado
+                {
+                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    /* ------------------------------------------------------------
+                     * reader.GetGuid(0) -> Obtém o valor da coluna [Id] como GUID
+                     * reader.GetString(1) -> Obtém o valor da coluna [Title] como string
+                     * 0: Índice da coluna [Id] (primeira coluna)
+                     * 1: Índice da coluna [Title] (segunda coluna)
+                     * ------------------------------------------------------------ */
+                }
+            }
+        }
+
+        /* -------------------------------------------------------------
+         * Utilizamos blocos 'using' para garantir que recursos como
+         * conexões e leitores sejam liberados adequadamente após o uso,
+         * evitando problemas como vazamento de memória.
+         * ------------------------------------------------------------- */
+    }
+}
