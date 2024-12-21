895fd68cd2a90e1ef5811557851fe8d47d0b5fa4
Samuel Ribeiro <samuel.ribeiro77f@gmail.com>
2024-12-21 17:07:52 -0400

    Testando WSL

--- Dapper/Bank/Program.cs
+++ Dapper/Bank/Program.cs
@@ -11,6 +11,54 @@ internal class Program
 		using (AppSettings.Connection = new SqlConnection(ConnectionString))
 		{
 			AppSettings.Connection.Open();
+			Load();
 		}
+	}
+
+	private static void Load()
+	{
+		Console.Clear();
+		Console.WriteLine("\u001b[32mBem-vindo ao Banco Luck\u001b[0m");
+		Console.WriteLine("Digite o dígito da opção desejada:");
+		Console.WriteLine("[1] - Acessar Conta");
+		Console.WriteLine("[2] - Criar Conta");
+		Console.WriteLine("[3] - Encerrar programa\n");
+		Console.Write("\u001b[32m> \u001b[0m");
+
+		if (!byte.TryParse(Console.ReadLine(), out byte value))
+			InvalidValue();
+
+		Directing(value);
+	}
+	
+	private static void Directing(byte value) 
+	{
+		switch(value) 
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				Closing();
+				break;
+			default:
+				InvalidValue(); 
+				break;
+		}
+	}
+	
+	private static void Closing() 
+	{
+		Console.Clear();
+		Console.WriteLine("\u001b[32mPrograma Encerrado!\u001b[0m");
+	}
+	
+	private static void InvalidValue() 
+	{
+		Console.Clear();
+		Console.WriteLine("\u001b[33mOpção Inválida...Informe um dos dígitos diponiveis!\u001b[0m");
+		Console.ReadKey();
+		Load();
 	}
 }
